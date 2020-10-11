f717ec02a9e28e5f4b36a60948748c87c408504f
liushuochen <2524453728@qq.com>
2020-10-12 00:07:04 +0800

    add user blue and index.html

---
+++ user/user.py
@@ -0,0 +1,14 @@
+# user routers
+from flask import Blueprint
+
+user_blue = Blueprint("user_blue", __name__, url_prefix="/user/v1")
+
+
+@user_blue.after_request
+def after_request(resp):
+    resp.headers.add('Access-Control-Allow-Headers',
+                     'Content-Type,Authorization,session_id')
+    resp.headers.add('Access-Control-Allow-Methods',
+                     'GET,PUT,POST,DELETE,OPTIONS,HEAD')
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
